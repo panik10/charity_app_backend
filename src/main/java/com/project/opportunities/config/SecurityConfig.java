459302f9e83b713854524afe61fe282aeaac22e5
Pavlo Nikolaiev <109036070+panik10@users.noreply.github.com>
2025-06-16 13:01:49 +0200

    Update SecurityConfig.java

--- src/main/java/com/project/opportunities/config/SecurityConfig.java
+++ src/main/java/com/project/opportunities/config/SecurityConfig.java
@@ -86,12 +86,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(
-                "http://localhost:[*]",
-                "http://127.0.0.1:[*]",
-                "https://charity-app-frontend.onrender.com:[*]",
-                "http://charity-app-frontend.onrender.com:[*]"
-        ));
+        configuration.setAllowedOrigins(List.of("https://charity-app-frontend.onrender.com"));
         configuration.setAllowedMethods(
                 Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(List.of("*"));
