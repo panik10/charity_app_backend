36fe0a83b57e7089a434d0aff6ba42bd9a17eb8c
Pavlo Nikolaiev <109036070+panik10@users.noreply.github.com>
2025-06-26 20:32:25 +0200

    Update SecurityConfig.java

--- src/main/java/com/project/opportunities/config/SecurityConfig.java
+++ src/main/java/com/project/opportunities/config/SecurityConfig.java
@@ -57,7 +57,6 @@ public class SecurityConfig {
                                         "/public/**",
                                         "/")
                                 .permitAll()
-                                .requestMatchers("/admin/**").hasRole("SUPER_ADMIN")
                                 .anyRequest()
                                 .authenticated()
                 )
